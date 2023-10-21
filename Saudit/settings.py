be206525ac5647420423881cf659516fd480c5ff
jawad amin khan <138467162+jawadamin4@users.noreply.github.com>
2023-10-21 17:15:33 +0500

    Update settings.py

--- Saudit/settings.py
+++ Saudit/settings.py
@@ -145,9 +145,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Default primary key field type
