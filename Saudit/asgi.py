42f31d336725467ff84cf718ee939c044006605f
jawadamin4 <jawadamin4567@gmail.com>
2023-11-11 12:36:16 +0500

    Update asgi.py

--- Saudit/asgi.py
+++ Saudit/asgi.py
@@ -6,20 +6,22 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
-
 import os
-
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter , URLRouter
-from fieldwork2 import routing
-from fieldwork2.routing import websocket_urlpatterns
 
+# Set the DJANGO_SETTINGS_MODULE environment variable
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Saudit.settings')
 
+# Load the ASGI application
+django_asgi_app = get_asgi_application()
+
+# Import your routing and WebSocket patterns after settings are configured
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from fieldwork2.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(websocket_urlpatterns)
     ),
