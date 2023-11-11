61fc1b041f1fbdc945e29ad4d32ce4582632b3c0
jawadamin4 <jawadamin4567@gmail.com>
2023-11-11 16:10:37 +0500

    Revert "standard"
    This reverts commit 10e73bcbfc03bb2ea231fff7bb203cb736aa5e81.

--- Saudit/asgi.py
+++ Saudit/asgi.py
@@ -13,6 +13,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Saudit.settings')
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 from fieldwork2.routing import websocket_urlpatterns
 
